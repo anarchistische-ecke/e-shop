2d9a0767558d3b37083db0673676deb6d3a3c64d
freddycooper <almosttanonymous@gmail.com>
2025-09-05 14:28:55 +0300

    initial commit.

---
+++ src/test/java/ru/postel_yug/eshop/PostelYugEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.postel_yug.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostelYugEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
